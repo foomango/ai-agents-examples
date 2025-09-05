35c9c0a142f8a520e741c646411a2e539f319f71
Fengwei Wang <foomango@gmail.com>
2025-09-05 08:51:22 -0400

    ✨ Change the interaction to chat

--- mce-agent/main.py
+++ mce-agent/main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from mce-agent!")
+from strands import Agent, tool
+from strands_tools import calculator # Import the calculator tool
+import argparse
+import json
+from strands.models import BedrockModel
 
+# Create a custom tool 
+@tool
+def weather():
+    """ Get weather """ # Dummy implementation
+    return "sunny"
+
+
+model_id = "us.anthropic.claude-3-7-sonnet-20250219-v1:0"
+model = BedrockModel(
+    model_id=model_id,
+)
+agent = Agent(
+    model=model,
+    tools=[calculator, weather],
+    system_prompt="You're a helpful assistant. You can do simple math calculation, and tell the weather."
+)
+
+def strands_agent_bedrock(payload):
+    """
+    Invoke the agent with a payload
+    """
+    user_input = payload.get("prompt")
+    response = agent(user_input)
+    return response.message['content'][0]['text']
 
 if __name__ == "__main__":
-    main()
+    while True:
+        user_input = input("\n🤵 User: ").strip()
+        if user_input.lower() in ["exit", "quit"]:
+            print("Exiting...")
+            break
+        if not user_input:
+            continue
+        print("🤖 Agent: ")
+        agent(user_input)
