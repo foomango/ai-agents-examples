242fe1c21405b304acb9e203ef5a4bd7b7b3a7f3
Fengwei Wang <foomango@gmail.com>
2025-09-05 15:25:36 -0400

    👷 Configure AgentCore Runtime deployment

--- 01-strands-with-bedrock-model/strands_claude.py
+++ 01-strands-with-bedrock-model/strands_claude.py
@@ -1,8 +1,9 @@
 from strands import Agent, tool
 from strands_tools import calculator # Import the calculator tool
-import argparse
-import json
 from strands.models import BedrockModel
+from bedrock_agentcore.runtime import BedrockAgentCoreApp
+
+app = BedrockAgentCoreApp()
 
 # Create a custom tool 
 @tool
@@ -21,16 +22,15 @@ agent = Agent(
     system_prompt="You're a helpful assistant. You can do simple math calculation, and tell the weather."
 )
 
+@app.entrypoint
 def strands_agent_bedrock(payload):
     """
     Invoke the agent with a payload
     """
     user_input = payload.get("prompt")
+    print("User input:", user_input)
     response = agent(user_input)
     return response.message['content'][0]['text']
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("payload", type=str)
-    args = parser.parse_args()
-    response = strands_agent_bedrock(json.loads(args.payload))
+    app.run()
